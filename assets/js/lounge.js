cf670105a9e25ddc34a80d25d80a336d7b0bc70b
gitneil1 <ronielson.agor1@gmail.com>
2016-08-03 22:41:44 +0800

    Add files via upload

--- assets/js/lounge.js
+++ assets/js/lounge.js
@@ -12,28 +12,32 @@ var elems = document.getElementById('questions');
     console.log('Username is: ' + username);
 
 lounge.controller('loungeCtrl', function($scope, $http){
-    //console.log('welcome');
     
     $scope.username = username;
+    $scope.testingCode = '00';
     
     $http.get('/getData/member').success(function(res){
-    
+        //not working -> testingCode
+        $scope.testingCode = "in getData";
         if(res.length > 0){
     
             for(var i = 0; i < res.length; i++){
             
-                    elems.insertAdjacentHTML('afterBegin', '<li><a href=\'/one-question-members/' + res[i].ques + '\'>' + res[i].ques + '</a></li>');
+                    elems.insertAdjacentHTML('afterBegin', '<li class="listOfcurrentQuestions" data-category="' + res[i].categories + '"><a href=\'/one-question-members/' + 
+                                             res[i].ques + '\'>' + 
+                                             res[i].ques + 
+                                             '</a></li>');
             }
-    
         }else{
             $scope.ques = "none";
         }
     });//end /getData
     
+    console.log('value of testingCode: ' + $scope.testingCode);
+    
     //put all categories in an array. to be maintainable if there's an expansion/modification
     //this is working
-    $scope.categories = [{name: '-No sort-'},
-                         {name: 'Science and Technology'},
+    $scope.categories = [{name: 'Science and Technology'},
                          {name: 'Business'},
                          {name: 'Personal'},
                          {name: 'Social'},
@@ -41,7 +45,23 @@ lounge.controller('loungeCtrl', function($scope, $http){
     
     //should sort/map the array of today's questions based on category
     $scope.update = function(){
-        console.log('Item name: ' + $scope.category.name);
+        //get all <li> with data-categories == selected value. should return an array if successful
+        var selectedElems = document.querySelectorAll('li.listOfcurrentQuestions[data-category="' + $scope.category.name + '"]');
+        for(var i = 0; i < selectedElems.length; i++){
+                //show all elements which are of the selected category
+                selectedElems[i].style.display = "block";
+            }
+        
+        //get all <li> with data-categories != selected value. should return an array if successful
+        var UnselectedElems = document.querySelectorAll('li.listOfcurrentQuestions:not([data-category="' + $scope.category.name + '"])');
+        if(UnselectedElems.length > 0){
+            //hide all elements which are not of the selected category
+            for(var i = 0; i < UnselectedElems.length; i++){
+                //hide all elements which are not of the selected category
+                UnselectedElems[i].style.display = "none";
+            }
+        }
+        
     }
     
 });// end loungeCtrl
