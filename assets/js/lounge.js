f4690906c449b446d381421b334c4b55f163671b
gitneil1 <ronielson.agor1@gmail.com>
2016-08-04 23:19:09 +0800

    Add files via upload

--- assets/js/lounge.js
+++ assets/js/lounge.js
@@ -12,28 +12,24 @@ var elems = document.getElementById('questions');
     console.log('Username is: ' + username);
 
 lounge.controller('loungeCtrl', function($scope, $http){
-    
     $scope.username = username;
-    $scope.testingCode = '00';
     
     $http.get('/getData/member').success(function(res){
-        //not working -> testingCode
-        $scope.testingCode = "in getData";
         if(res.length > 0){
     
             for(var i = 0; i < res.length; i++){
             
-                    elems.insertAdjacentHTML('afterBegin', '<li class="listOfcurrentQuestions" data-category="' + res[i].categories + '"><a href=\'/one-question-members/' + 
+                    elems.insertAdjacentHTML('afterBegin', '<li class="listOfcurrentQuestions" data-category="' + 
+                                             res[i].categories + '"><a href=\'/one-question-members/' + 
                                              res[i].ques + '\'>' + 
                                              res[i].ques + 
                                              '</a></li>');
             }
         }else{
-            $scope.ques = "none";
+            //if there's no question for the day, post a default message
+            elems.insertAdjacentHTML('afterBegin', '<li>There are no questions posted today.</li>');
         }
     });//end /getData
-    
-    console.log('value of testingCode: ' + $scope.testingCode);
     
     //put all categories in an array. to be maintainable if there's an expansion/modification
     //this is working
@@ -47,6 +43,8 @@ lounge.controller('loungeCtrl', function($scope, $http){
     $scope.update = function(){
         //get all <li> with data-categories == selected value. should return an array if successful
         var selectedElems = document.querySelectorAll('li.listOfcurrentQuestions[data-category="' + $scope.category.name + '"]');
+        
+        
         for(var i = 0; i < selectedElems.length; i++){
                 //show all elements which are of the selected category
                 selectedElems[i].style.display = "block";
