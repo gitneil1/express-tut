5082c409b2076fd38eb4bc88ea85917a01279d3a
gitneil1 <ronielson.agor1@gmail.com>
2016-08-09 13:05:30 +0800

    Add files via upload

--- assets/js/lounge.js
+++ assets/js/lounge.js
@@ -9,7 +9,7 @@ var elems = document.getElementById('questions');
     for(var i = 0; i < cookieArray.length; i++){
         var username = cookieArray[i].split('=')[1];
     }
-    console.log('Username is: ' + username);
+    //console.log('Username is: ' + username);
 
 lounge.controller('loungeCtrl', function($scope, $http){
     $scope.username = username;
@@ -41,22 +41,7 @@ lounge.controller('loungeCtrl', function($scope, $http){
     
     //should sort/map the array of today's questions based on category
     $scope.update = function(){
-        //get all <li> with data-categories == selected value. should return an array if successful
-        var selectedElems = document.querySelectorAll('li.listOfcurrentQuestions[data-category="' + $scope.category.name + '"]');
         
-        if(selectedElems.length > 0){
-            for(var i = 0; i < selectedElems.length; i++){
-                //show all elements which are of the selected category
-                selectedElems[i].style.display = "block";
-            }
-        }else{
-            //remove all child elements of elems
-            while(elems.firstChild){
-                elems.removeChild(elems.firstChild);
-            }
-            //insert a default message
-            elems.insertAdjacentHTML('afterBegin', '<li>There are no questions with that category.</li>');
-        }
         //get all <li> with data-categories != selected value. should return an array if successful
         var UnselectedElems = document.querySelectorAll('li.listOfcurrentQuestions:not([data-category="' + $scope.category.name + '"])');
         if(UnselectedElems.length > 0){
@@ -66,6 +51,33 @@ lounge.controller('loungeCtrl', function($scope, $http){
                 UnselectedElems[i].style.display = "none";
             }
         }
+        
+        //get all <li> with data-categories == selected value. should return an array if successful
+        var selectedElems = document.querySelectorAll('li.listOfcurrentQuestions[data-category="' + $scope.category.name + '"]');
+        
+        if(selectedElems.length > 0){
+            //check if #noResultCategory exists. if it does, remove it
+            var noResult = document.getElementById('noResultCategory');
+            if(noResult){
+               elems.removeChild(noResult) ;
+            }
+            
+            for(var i = 0; i < selectedElems.length; i++){
+                //show all elements which are of the selected category
+                selectedElems[i].style.display = "block";
+            }
+        }else{
+            //selected category returns null. create <li> to contain default message
+            if(!document.getElementById('noResultCategory')){
+                var noResultLi = document.createElement('li');
+                noResultLi.setAttribute('id', 'noResultCategory');
+                var defaultMsg = document.createTextNode('There are no questions with that category.');
+                noResultLi.appendChild(defaultMsg);
+
+                elems.appendChild(noResultLi);
+            }
+        }
+        
         
     }
     
